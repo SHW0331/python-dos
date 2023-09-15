ef7a819b1a1695a2bccbbb7a3d9e99e095edd0f8
bite98 <sywok19980331@gmail.com>
2023-09-15 22:45:43 +0900

    00

--- lecture/python/python-web/webProject/common/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-
-urlpatterns = [
-    path("login/", LoginView.as_view(template_name="common/login.html")),
-
-]
--- lecture/python/work/3.py
+++
@@ -1,9 +0,0 @@
-import test1
-import test2
-
-test = test1.add()
-abc = test2.min()
-
-result = test + abc
-print(result)
-
